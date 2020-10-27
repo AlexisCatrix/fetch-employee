dd1327243e9a33836947f44f84c99bdf5091ddd8
AlexisCatrix <alexis.4x.ac@gmail.com>
2020-10-27 20:04:31 +0100

    Made a fetch employee web App through an API

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import { Component } from "react";
+import axios from "axios";
+import "./App.css";
+import DisplayEmployee from "./Components/DisplayEmployee";
+import sampleEmployee from "./sampleEmployee";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      employee: sampleEmployee,
+    };
+    this.getEmployee = this.getEmployee.bind(this);
+  }
+
+  getEmployee() {
+    // Send the request
+    axios
+      .get("https://randomuser.me/api?nat=US")
+      // Extract the DATA from the received response
+      .then((response) => response.data)
+      // Use this data to update the state
+      .then((data) => {
+        this.setState({
+          // loading: false,
+          employee: data.results[0],
+        });
+      });
+  }
+
+  render() {
+    return (
+      <div>
+        <DisplayEmployee employee={this.state.employee} />
+        <button type="button" onClick={this.getEmployee}>
+          Get employee
+        </button>
+      </div>
+    );
+  }
 }
 
 export default App;
