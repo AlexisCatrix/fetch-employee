9467aef5eb37adddf19e26f6926a239de03f199e
AlexisCatrix <alexis.4x.ac@gmail.com>
2020-11-18 14:52:54 +0100

    Transform Class component to function component with hooks

--- src/App.js
+++ src/App.js
@@ -1,51 +1,83 @@
-import { Component } from "react";
+import { useState, useEffect } from "react";
 import axios from "axios";
 import "./App.css";
 import DisplayEmployee from "./Components/DisplayEmployee";
 import sampleEmployee from "./sampleEmployee";
 import LoadingSpinner from "./LoadingSpinner";
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      employee: sampleEmployee,
-    };
-    this.getEmployee = this.getEmployee.bind(this);
-  }
+export default function App() {
+  const [employee, setEmployee] = useState(sampleEmployee);
+  const [loading, setLoading] = useState(true);
 
-  componentDidMount(){
-    this.getEmployee();
-  }
+  useEffect(() => {
+    getEmployee();
+  }, []);
 
-  getEmployee() {
+  const getEmployee = () => {
     // Send the request
-    this.setState({ loading: true }, () => { axios
+    setLoading(true);
+    axios
       .get("https://randomuser.me/api?nat=US")
       // Extract the DATA from the received response
-      .then((response) => response.data)
-      // Use this data to update the state
-      .then((data) => {
-        this.setState({
-          // loading: false,
-          employee: data.results[0],
-          loading: false,
-        });
-      });})
-   
-  }
+      .then(({ data }) => {
+        setEmployee(data.results[0]);
+        setLoading(false);
+        // loading: false,
+      });
+  };
 
-  render() {
-    const {loading } = this.state;
-    return (
-      <div>
-        {loading ? <LoadingSpinner /> : <DisplayEmployee employee={this.state.employee}/>}
-        <button type="button" onClick={this.getEmployee}>
-          Get employee
-        </button>
-      </div>
-    );
-  }
+  return (
+    <div>
+      {loading ? (
+        <LoadingSpinner />
+      ) : (
+        <DisplayEmployee employee={employee} />
+      )}
+      <button type="button" onClick={getEmployee}>
+        Get employee
+      </button>
+    </div>
+  );
 }
 
-export default App;
+// class App extends Component {
+//   componentDidMount() {
+//     this.getEmployee();
+//   }
+
+  // getEmployee() {
+  //   // Send the request
+  //   this.setState({ loading: true }, () => {
+  //     axios
+  //       .get("https://randomuser.me/api?nat=US")
+  //       // Extract the DATA from the received response
+  //       .then((response) => response.data)
+  //       // Use this data to update the state
+  //       .then((data) => {
+  //         this.setState({
+  //           // loading: false,
+  //           employee: data.results[0],
+  //           loading: false,
+  //         });
+  //       });
+  //   });
+  // }
+
+//   render() {
+//     const { loading } = this.state;
+//     return (
+//       <div>
+//         {loading ? (
+//           <LoadingSpinner />
+//         ) : (
+//           <DisplayEmployee employee={this.state.employee} />
+//         )}
+//         <button type="button" onClick={this.getEmployee}>
+//           Get employee
+//         </button>
+//       </div>
+//     );
+//   }
+// }
+
+// export default App;
